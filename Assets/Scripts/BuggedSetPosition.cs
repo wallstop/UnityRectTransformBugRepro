879f82398b937d5d6f998ed3d505a756acc6ff80
wallstop <dbfsinfil@gmail.com>
2018-04-03 18:59:40 -0700

    Initial commit

---
+++ Assets/Scripts/BuggedSetPosition.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public sealed class BuggedSetPosition : MonoBehaviour
+    {
+        private void Start()
+        {
+            SpawnChildren();
+        }
+
+        private void SpawnChildren()
+        {
+            GameObject child = new GameObject("Child", typeof (RectTransform));
+
+            RectTransform childTransform = child.GetComponent<RectTransform>();
+            Debug.Log(string.Format("Bugged - initial position: {0}.", childTransform.localPosition));
+            childTransform.SetParent(transform, false);
+            childTransform.localPosition = new Vector3(1000, 100, 4);
+            childTransform.sizeDelta = new Vector2(1, 1); // localPosition is now (0, 0, 4)
+            Debug.Log(string.Format("Bugged - final position: {0}.", childTransform.localPosition));
+        }
+    }
+}
---
+++ Assets/Scripts/NotBuggedSetPosition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class NotBuggedSetPosition : MonoBehaviour
+    {
+        private void Start()
+        {
+            SpawnChildren();
+        }
+
+        private void SpawnChildren()
+        {
+            GameObject child = new GameObject("Child", typeof (RectTransform));
+
+            RectTransform childTransform = child.GetComponent<RectTransform>();
+
+            childTransform.SetParent(transform, false);
+            childTransform.sizeDelta =
+                new Vector2(1, 1); // Changing sizeDelta prior to localPosition update makes localPosition stick
+            Debug.Log(string.Format("NotBugged - initial position: {0}.", childTransform.localPosition));
+            childTransform.localPosition = new Vector3(1000, 100, 4);
+            childTransform.sizeDelta = new Vector2(1, 1); // localPosition should be the same.
+            Debug.Log(string.Format("NotBugged - final position: {0}.", childTransform.localPosition));
+        }
+    }
+}
